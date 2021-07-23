89e394abf4bb9f51d380969020fb10dfde2dfbf1
saron118965 <delinafiyona@gmail.com>
2021-07-22 18:42:17 -0700

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,8 @@ public class GitCool {
     // B23 IS LEARNING GIT
     //B23 IS A SPECIAL NUMBER
     //B23 IS MAKING SOME SENSE NOW
+
+    // We have added our Github account under intellij
+    //make sure to click apply and click on to save
 }
+
